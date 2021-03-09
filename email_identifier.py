6dbf0efa95a7e073e97d882568ab1b01ab31fa40
Wayne <swmask@gmail.com>
2021-03-09 13:41:57 -0500

    Cleaned up the code and added options for different approaches

--- email_identifier.py
+++ email_identifier.py
@@ -7,11 +7,31 @@ is_email = False
 for letter in Username:
     if letter == at_sign:
         is_email = True
-        
-if is_email == True:
-    User = Username.partition(at_sign)[0] + Subscription
-    print(User)
+        break
+    User += letter
     
-else:
-    Username += Subscription
-    print(Username)
+User += Subscription
+print(User)
+
+## Option 2 - (Find where the @ is and use it's location to determine what to keep from the string)
+#for i in range(len(Username)):
+#    if Username[i] == at_sign:
+#        is_email = True
+#        break
+#if is_email == True:
+#    User = Username[0:i]
+#    Username = User + Subscription
+#    print(Username)
+
+## Option 3 - (Use the split function)
+#if is_email == True:
+  #  Username = Username.split('@')[0] + Subscription
+  #  print(Username)
+
+## Option 4 - (Use partition function)
+#if is_email == True:
+  #  Username = Username.partition(at_sign)[0] + Subscription
+  #  print(Username)
+#else:
+#    Username += Subscription
+#    print(Username)
